66846b70e61495cebd0a1b40f6e542b6ff024eae
kmj <myng25@naver.com>
2025-05-14 22:54:31 +0900

    [Feat] Market.jsx에 컴포넌트 연동

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer'
+import Market from './pages/Market'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <Routes>
+        <Route path="/market" element={<Market />} />
       </Routes>
       <Footer />
     </>
--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -1,1 +1,14 @@
-// Market.jsx
+import MarketSection from "../components/MarketSection";
+import MarketList from "../components/MarketList";
+
+function Market() {
+  return (
+    <>
+      <MarketSection />
+      <hr className="border-t border-gray-300 mb-6" />
+      <MarketList />
+    </>
+  );
+}
+
+export default Market;
