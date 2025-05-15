3fa0deb09233d657b589628dfb7f749e3073d7ef
kmj <myng25@naver.com>
2025-05-16 03:10:43 +0900

    [Fix] import 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import Footer from './components/Footer'
-import MarketList from './components/MarketList';
+import Market from './pages/Market';
 import ProductDetail from './pages/ProductDetail'
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/market" element={<MarketList />} />
+        <Route path="/market" element={<Market />} />
         <Route path="/market/:producdId" element={<ProductDetail />} /> 
       </Routes>
       <Footer />
