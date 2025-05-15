219d1335bb352ab419a618ba061738d65667b37a
kingluminance <ntv93575@gmail.com>
2025-05-15 21:49:48 +0900

    [feat]:회원가입 디자인추가

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
+import "../App.css";
 
 function SignupForm() {
   const {
@@ -39,54 +40,65 @@ function SignupForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* 이름 인풋 */}
-      <div>
-        <label>이름</label>
-        <input
-          type="text"
-          {...register("name", { required: "이름은 필수입니다." })}
-        />
-        {errors.name && (
-          <p className="text-red-500 text-sm">{errors.name.message}</p>
-        )}
-      </div>
-      {/* 이메일 */}
-      <div>
-        <label>이메일</label>
-        <input
-          type="email"
-          {...register("email", {
-            required: "이메일은 필수입니다.",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
-              message: "유효한 이메일 주소를 입력해주세요.",
-            },
-          })}
-        />
-        {errors.email && (
-          <p className="text-red-500 text-sm">{errors.email.message}</p>
-        )}
-      </div>
-      {/* 비밀번호 */}
-      <div>
-        <label>비밀번호</label>
-        <input
-          type="password"
-          {...register("password", {
-            required: "비밀번호는 필수입니다.",
-            minLength: {
-              value: 6,
-              message: "비밀번호는 최소 6자 이상이어야 합니다.",
-            },
-          })}
-        />
-        {errors.password && (
-          <p className="text-red-500 text-sm">{errors.password.message}</p>
-        )}
-      </div>
-      <button type="submit">회원가입</button>
-    </form>
+    <div className="login-box">
+      <h1 className="login-title">회원가입</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="login-box-right">
+        {/* 이름 인풋 */}
+        <div>
+          <label className="login-field">이름</label>
+          <input
+            placeholder="Enter your full name"
+            className="login-input"
+            type="text"
+            {...register("name", { required: "이름은 필수입니다." })}
+          />
+          {errors.name && (
+            <p className="text-red-500 text-sm">{errors.name.message}</p>
+          )}
+        </div>
+        {/* 이메일 */}
+        <div>
+          <label className="login-field">Email</label>
+          <input
+            placeholder="Enter your email"
+            className="login-input"
+            type="email"
+            {...register("email", {
+              required: "이메일은 필수입니다.",
+              pattern: {
+                value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+                message: "유효한 이메일 주소를 입력해주세요.",
+              },
+            })}
+          />
+          {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email.message}</p>
+          )}
+        </div>
+        {/* 비밀번호 */}
+        <div>
+          <label className="login-field">비밀번호</label>
+          <input
+            placeholder="Create a password"
+            className="login-input"
+            type="password"
+            {...register("password", {
+              required: "비밀번호는 필수입니다.",
+              minLength: {
+                value: 6,
+                message: "비밀번호는 최소 6자 이상이어야 합니다.",
+              },
+            })}
+          />
+          {errors.password && (
+            <p className="text-red-500 text-sm">{errors.password.message}</p>
+          )}
+        </div>
+        <button type="submit" className="login-button">
+          <p>회원가입</p>
+        </button>
+      </form>
+    </div>
   );
 }
 
