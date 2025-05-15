db77662d725b9f0d189277cca73c55ea2f17b2b3
kmj <myng25@naver.com>
2025-05-15 21:33:16 +0900

    [Fix] Mock Data 기반 코드 수정

--- src/components/MarketList.jsx
+++ src/components/MarketList.jsx
@@ -1,4 +1,5 @@
 import mockProducts from '/src/api/mockData';
+import { Link } from 'react-router-dom';
 
 function MarketList() {
   const products = mockProducts;
@@ -9,31 +10,29 @@ function MarketList() {
       <p className="text-sm text-gray-500 text-center mb-6">
         여기에서 판매되는 농작물 상품들을 확인하세요.
       </p>
-
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {products.map((item) => (
-          <div
-            key={item.id}
-            className="border rounded-lg shadow-sm p-4 hover:shadow-md transition bg-white"
-          >
-            {/* 라벨 */}
-            <span className="inline-block text-xs text-gray-600 bg-gray-100 px-2 py-1 rounded mb-2">
-              {item.label}
-            </span>
+        {products.map((item, index) => (
+          <Link to={`/market/${item.produceId}`} key={`${item.produceId}-${index}`}>
+            <div className="border rounded-lg shadow-sm p-4 hover:shadow-md transition bg-white">
+              {/* 카테고리 */}
+              <span className="inline-block text-xs text-gray-600 bg-gray-100 px-2 py-1 rounded mb-2">
+                {item.category}
+              </span>
 
-            {/* 이미지 대체 박스 */}
-            <div className="h-32 bg-gray-100 flex items-center justify-center text-gray-400 text-sm mb-3">
-              {item.description}
+              {/* 이미지 */}
+              <div className="h-32 bg-gray-100 flex items-center justify-center text-gray-400 text-sm mb-3">
+                <img src={item.images} alt={item.name} className="h-full w-full object-cover" />
+              </div>
+
+              {/* 상품 이름 */}
+              <h3 className="font-medium mb-1">{item.title}</h3>
+
+              {/* 가격 */}
+              <p className="text-sm text-gray-700">
+                가격: <span className="font-bold">{item.price.toLocaleString()}원</span>
+              </p>
             </div>
-
-            {/* 상품 이름 */}
-            <h3 className="font-medium mb-1">{item.name}</h3>
-
-            {/* 가격 */}
-            <p className="text-sm text-gray-700">
-              가격: <span className="font-bold">{item.price.toLocaleString()}원</span>
-            </p>
-          </div>
+          </Link>
         ))}
       </div>
     </section>
