8bfafea0e13de53bbb15dc9ddab28e26f5fdadec
kmj <myng25@naver.com>
2025-05-16 19:58:50 +0900

    [Feat] 문의하기 UI 구현

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+export default function Contact() {
+  return (
+    <div>
+
+
+      
+      <section className="text-left mt-12 px-6 pb-20">
+        <h2 className="text-xl font-bold mb-2">문의하기</h2>
+        <p className="text-sm text-gray-500 mb-4">Farm2You에 문의해주세요.</p>
+        <button className="px-6 py-2 bg-black text-white rounded">문의하기</button>
+      </section>
+    </div>
+  );
+}
+
+
+console.log("Contact page loaded");
