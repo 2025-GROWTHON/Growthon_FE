4c972d9a202c1c4edf4b80c99f532708a5686611
kmj <myng25@naver.com>
2025-05-20 16:56:27 +0900

    [Feat] 상세페이지로 접근 기능

--- src/components/LatestProduct.jsx
+++ src/components/LatestProduct.jsx
@@ -1,28 +1,27 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function LatestProducts() {
   const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-  axios.get('/api/produces')
-    .then((res) => {
-      console.log('응답 구조 확인:', res.data);
-
-      if (res.data.status === 200) {
-        const sorted = [...res.data.data].sort(
-          (a, b) => new Date(b.updateAt) - new Date(a.updateAt)
-        );
-        setProducts(sorted);
-        console.log(res.data.message);
-      } else {
-        console.error('API 오류 또는 data 없음:', res.data.message);
-      }
-    })
-    .catch((err) => {
-      console.error('요청 실패:', err);
-    });
-}, []);
+    axios.get('/api/produces')
+      .then((res) => {
+        if (res.data.status === 200) {
+          const sorted = [...res.data.data].sort(
+            (a, b) => new Date(b.updateAt) - new Date(a.updateAt)
+          );
+          setProducts(sorted);
+        } else {
+          console.error('API 오류 또는 data 없음:', res.data.message);
+        }
+      })
+      .catch((err) => {
+        console.error('요청 실패:', err);
+      });
+  }, []);
 
   return (
     <div className="my-10 text-center">
@@ -32,7 +31,8 @@ export default function LatestProducts() {
         {products.map((product) => (
           <div
             key={product.produceId}
-            className="border rounded-lg shadow p-6 text-left min-h-[260px] flex flex-col justify-between"
+            className="border rounded-lg shadow p-6 text-left min-h-[260px] flex flex-col justify-between cursor-pointer hover:shadow-lg transition"
+            onClick={() => navigate(`/market/${product.produceId}`)}
           >
             <div className="text-sm text-gray-400 mb-1">{product.category}</div>
             <div className="bg-gray-200 h-40 flex items-center justify-center mb-3 overflow-hidden">
