4cd1a99098be9c3baf17d36dea661b9028c3f8c6
kingluminance <ntv93575@gmail.com>
2025-05-23 18:07:44 +0900

    [Feat]:상품수정선택디자인

--- src/components/Myproduct.jsx
+++ src/components/Myproduct.jsx
@@ -2,30 +2,29 @@ import React from "react";
 
 function Myproduct({ item }) {
   return (
-    <div>
-      <div className="border rounded-lg shadow-sm p-4 hover:shadow-md transition bg-white">
-        {/* 카테고리 */}
-        <span className="inline-block text-xs text-gray-600 bg-gray-100 px-2 py-1 rounded mb-2">
-          {item.category}
-        </span>
+    <div className="myProduct-card">
+      <div className="myProduct-card-in">
+        <div className="myProduct-top">
+          {/* 카테고리 */}
+          <div className="myProduct-card-category">{item.category}</div>
 
-        {/* 이미지 */}
-        <div className="h-32 bg-gray-100 flex items-center justify-center text-gray-400 text-sm mb-3">
-          <img
-            src={`http://localhost:8080/images/${item.images}`}
-            alt={item.title}
-            className="h-full w-full object-cover"
-          />
+          {/* 이미지 */}
+          <div>
+            <img
+              src={`http://localhost:8080/images/${item.images}`}
+              alt={item.title}
+            />
+          </div>
         </div>
+        <div className="porduct-dis">
+          {/* 상품 이름 */}
+          <h3>{item.title}</h3>
 
-        {/* 상품 이름 */}
-        <h3 className="font-medium mb-1">{item.title}</h3>
-
-        {/* 가격 */}
-        <p className="text-sm text-gray-700">
-          가격:{" "}
-          <span className="font-bold">{item.price.toLocaleString()}원</span>
-        </p>
+          {/* 가격 */}
+          <p>
+            가격: <span>{item.price.toLocaleString()}원</span>
+          </p>
+        </div>
       </div>
     </div>
   );
